7b69dac854596c9ca77a9973b174a2ee73c85cd0
落落佳一、 <1780406643@qq.com>
2018-09-04 19:53:14 +0800

    Update 010  矩形覆盖.cpp

--- 010  矩形覆盖.cpp
+++ 010  矩形覆盖.cpp
@@ -4,3 +4,29 @@
 */
 //author = Qiufeng
 
+class Solution {
+public:
+    int rectCover(int number) {
+        vector<int> way;
+        int temp;
+        int i;
+        if(number==0)
+        {
+            return 0;
+        }
+        
+        for (i = 1; i <= number; i++)
+	    {
+            if (i == 1)
+                way.push_back(1);
+            else if (i == 2)
+                way.push_back(2);
+            else 
+            {
+                temp = way[way.size() - 1] + way[way.size() - 2];
+                way.push_back(temp);
+            }
+	     }
+        return way[way.size()-1];
+    }
+};
