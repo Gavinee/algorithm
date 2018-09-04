c7d66f3ee73a92123d53d4dec734e830428ed8f0
落落佳一、 <1780406643@qq.com>
2018-09-04 19:06:06 +0800

    Update 009  变态跳台阶.cpp

--- 009  变态跳台阶.cpp
+++ 009  变态跳台阶.cpp
@@ -4,6 +4,14 @@
 */
 //author = Qiufeng
 
+/*
+  思路：递推
+  当number = 1时,way = 1
+  当number = 2时，way = 2
+  当number = 3时,way = 4   ,当最后一步为跨一步台阶，则way应为2,当最后一步为跨两步时，则way应为1,再可以一次跨n步，n = 3,则一共有2+1+1=4种跳法
+  将所有小于number的way累加，再加上一步登天的一种方法，则共有pow(2,number-1)种跳法.
+*/
+
 #include<cmath>
 class Solution {
 public:
