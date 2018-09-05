446456b808d2d0d01b39e4d6ac2e04907f08fd6e
落落佳一、 <1780406643@qq.com>
2018-09-05 18:00:45 +0800

    Create 012  数值的整数次方.cpp

---
+++ 012  数值的整数次方.cpp
@@ -0,0 +1,14 @@
+/*
+  题目描述
+  给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+*/
+
+// author = Qiufeng
+
+#include<cmath>
+class Solution {
+public:
+    double Power(double base, int exponent) {
+        return pow(base,exponent);
+    }
+};
