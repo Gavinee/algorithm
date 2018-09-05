83fc424a82d1a0c387b212cbabcde7017ac8331f
落落佳一、 <1780406643@qq.com>
2018-09-05 20:28:40 +0800

    Update 014  链表中倒数第k个结点.cpp

--- 014  链表中倒数第k个结点.cpp
+++ 014  链表中倒数第k个结点.cpp
@@ -4,3 +4,42 @@
 */
 
 // author = Qiufeng
+
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* FindKthToTail(ListNode* pListHead, unsigned int k) {
+        ListNode* node;
+        ListNode* temp = pListHead;
+        unsigned int count = 1;
+        
+        if(temp==NULL)
+            return NULL;           //不能返回node,否则结点中自动生成random元素。造成结果错误。
+        while(temp->next!=NULL)
+        {
+            temp = temp->next;
+            count++;
+        }
+        
+        if(k>count)
+            return NULL;
+        else
+        {
+            count = count - k;
+            while(count>0)
+            {
+                pListHead = pListHead->next;
+                count--;
+            }
+            node = pListHead;
+            return node;
+        }
+    }
+};
