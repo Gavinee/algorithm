bfdcf9cb44040d552a595540801b82b8e53a1d90
落落佳一、 <1780406643@qq.com>
2018-09-06 07:06:53 +0800

    Update 016  合并两个排序的链表.cpp

--- 016  合并两个排序的链表.cpp
+++ 016  合并两个排序的链表.cpp
@@ -51,48 +51,51 @@ class Solution {
 public:
 	ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
 	{
-
-		if (pHead1 == nullptr and pHead2 == nullptr)
-			return NULL;
-		else if (pHead1 == nullptr)
-			return pHead2;
-		else if (pHead2 == nullptr)
-			return pHead1;
-		else
-		{
-			ListNode *tt, *temp;
-			tt = new ListNode(0);
-			tt->next = nullptr;
-			temp = tt;
-			while (pHead1 != nullptr and pHead2 != nullptr)
-			{
-
-				if (pHead1->val < pHead2->val)
-				{
-					temp->next = pHead1;
-					pHead1 = pHead1->next;
-				}
-				else
-				{
-					temp->next = pHead2;
-					pHead2 = pHead2->next;
-				}
-				temp = temp->next;
-			}
-			while (pHead2 != nullptr)
-			{
-				temp->next = pHead2;
-				temp = temp->next;
-				pHead2 = pHead2->next;
-			}
-			while (pHead1 != nullptr)
-			{
-				temp->next = pHead1;
-				temp = temp->next;
-				pHead1 = pHead1->next;
-			}
-			return tt->next;
-		}
+        if(pHead1 == nullptr and pHead2 == nullptr)
+            return NULL;
+        else if(pHead1!=nullptr and pHead2 == nullptr)
+            return pHead1;
+        else if(pHead1 == nullptr and pHead2 != nullptr)
+            return pHead2;
+        else{
+            ListNode* tt = new ListNode(0);
+            tt->next = nullptr; 
+            ListNode* temp;
+            temp = tt;      //为链表头开辟一个头结点
+            while(pHead1!=nullptr or pHead2!=nullptr)
+                {
+                    if(pHead1!=nullptr and pHead2!=nullptr and pHead1->val > pHead2->val)
+                    {
+                        temp->next = pHead2;
+                        pHead2 = pHead2->next;
+                        temp = temp->next;        //temp只是单个结点
+                    }
+                    else if(pHead1!=nullptr and pHead2!=nullptr and pHead1->val <= pHead2->val)
+                    {
+                        temp->next = pHead1;
+                        pHead1 = pHead1->next;
+                        temp = temp->next;
+                    }
+                    else if(pHead1==nullptr and  pHead2!=nullptr)
+                    {
+                        temp->next = pHead2;
+                        pHead2 = pHead2->next;
+                        temp = temp->next;
+                    }
+                    else if(pHead1!=nullptr and  pHead2 ==nullptr)
+                    {
+                        temp->next = pHead1;
+                        pHead1 = pHead1->next;
+                        temp = temp->next;
+                    }
+                    else
+                    {
+                        temp->next = nullptr;
+                    }
+                }
+            return tt->next;        //temp只是链表的单个结点，而tt则是整个链表，
+                                    //在输出合成后的链表，则需要删除第一个头结点
+            }
 	}
 };
 
