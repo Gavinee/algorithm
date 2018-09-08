f734c516922834c489fc5b63579416a3f57ca64a
落落佳一、 <1780406643@qq.com>
2018-09-08 10:30:08 +0800

    Update 047  求1+2+3+...+n.cpp

--- 047  求1+2+3+...+n.cpp
+++ 047  求1+2+3+...+n.cpp
@@ -1,6 +1,6 @@
-
 /*
-
+  题目描述
+  求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
 */
 //author = Qiufeng
 
@@ -8,9 +8,9 @@ class Solution {
 public:
     int Sum_Solution(int n) {
         int flag;
-		int add = 1;
-		flag = (n - 1) & -1;
-		flag && (add = n + Sum_Solution(n - 1));
-		return add;
+	int add = 1;
+	flag = (n - 1) & -1;
+	flag && (add = n + Sum_Solution(n - 1));
+	return add;
     }
 };
