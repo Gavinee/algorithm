3cd668148f188b420f3dec81a532dd6857a208a0
落落佳一、 <1780406643@qq.com>
2018-09-14 18:31:38 +0800

    Update 求根号值.cpp

--- 求根号值.cpp
+++ 求根号值.cpp
@@ -3,7 +3,8 @@
 */
 
 //author = Qiufeng
-
+//第一种方法想把范围缩放到1的大小再进行二分法。对于小数，方法一方法二的效率差不多
+//但一旦输入数据变得很大，则方法二的效率要大于方法一的效率。
 /****************************方法一***************************************/
 #include<vector>
 #include<iostream>
@@ -52,5 +53,39 @@ double dichotomy(int target,double delta)
 }
 
 
-/*****************************优化后***********************************/
+/*****************************方法二***********************************/
+#include<vector>
+#include<iostream>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+double dichotomy(int target,double delta);
+int main(void)
+{
+	double tt;
+	int ttx = 101;
+	double delta = 0.0001;
+	tt = dichotomy(ttx,delta);
+	cout << tt << endl;
+	system("pause");
+	return 0;
+}
 
+double dichotomy(int target,double delta)
+{
+	double value;
+	double start = 1, end = target;
+	double temp;
+	value = ((start + end) / 2)*((start + end) / 2);
+
+	while (abs(value - target) > delta)
+	{
+		temp = (start + end) / 2;
+		value = temp*temp;
+		if (value > target)
+			end = temp;
+		else
+			start = temp;
+	}
+	return temp;
+}
