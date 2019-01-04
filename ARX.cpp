ccce03ac1482850e308511a57a9df65253fa252f
落落佳一、 <1780406643@qq.com>
2019-01-04 17:53:31 +0800

    Create ARX.cpp

---
+++ ARX.cpp
@@ -0,0 +1,36 @@
+    #include<aced.h>				//ARX应用程序定义和访问AutoCAD指定服务所需要的头文件
+    #include<rxregsvc.h>			//acrxXXX工具函数所需的头文件
+
+    void initApp();					//在加载时所调用
+    void unloadApp();			    //在程序被卸载时所调用
+
+    void HelloWorld();
+
+    void initApp()
+    {
+      acedRegCmds->addCommand(
+        "Hello1",
+        "Hello",			//输入这两个命令名称均可以在AutoCAD中运行程序
+        "Hello",			
+        ACRX_CMD_MODAL,	    //模态命令	
+        HelloWorld
+        );
+    }
+
+    virtual Acad::ErrorStatus addCommand(
+      const char* cmdGroupName,			//指定命令组名称
+      const char* cmdGlobalName,		    //命令的国际名称
+      const char* cmdLocalName,			//命令的本国名称
+      Adesk::Int32 commandFlags,			//命令的类型
+      AcRxFunctionPtr FunctionAddr,	    //指向实现函数的指针
+      AcEdUIContext* UIContext = NULL,
+      int fcode = -1,
+      HINSTANCE hResourceHandle = NULL,
+      AcEdCommand** cmdPtrRet = NULL) = 0;
+
+    commandFlags是用来说明命令类型的   (1)模态命令  模态命令在执行过程中无法运行其他的模态命令，但是可以运行透明命令
+                       (2)透明命令	 
+
+
+
+
